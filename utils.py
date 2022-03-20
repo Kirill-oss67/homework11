5890efbbac33b2fb4dbea864976d5c0aca3b7b55
Kirill-oss67 <Yakovkirill-roslavl@yandex.ru>
2022-03-20 16:56:06 +0300

    Первая часть д/з

---
+++ utils.py
@@ -0,0 +1,8 @@
+def load_candidates_from_json(path):
+    pass
+def get_candidate(candidate_id):
+    pass
+def get_candidates_by_name(candidate_name):
+    pass
+def get_candidates_by_skill(skill_name):
+    pass
